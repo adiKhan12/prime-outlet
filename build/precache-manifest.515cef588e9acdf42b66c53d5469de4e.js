f4d085b0cd0b3d5b9e2e59d08c97e50f3ed37edc
Adnan Khan <Ghories555@gmail.com>
2023-03-01 15:05:33 +0100

    initial commit

---
+++ build/precache-manifest.515cef588e9acdf42b66c53d5469de4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148beb445f3831248e7d2e731d229551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dc725cb8da430108151",
+    "url": "/static/css/main.d3f13e23.chunk.css"
+  },
+  {
+    "revision": "7459fa95ee0db6480aa4",
+    "url": "/static/js/2.0cca92c5.chunk.js"
+  },
+  {
+    "revision": "038db422c0414d8dfd31541a1bf2a15e",
+    "url": "/static/js/2.0cca92c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc725cb8da430108151",
+    "url": "/static/js/main.ed5fdfe8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ed5fdfe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
